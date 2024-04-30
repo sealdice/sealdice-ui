a60f3763caa3775bd8d844c02386fdbf1a7f5cbc
JustAnotherID <just-another-id@outlook.com>
2024-04-30 13:36:14 +0800

    feat(Tool): 将指令测试调整到子菜单

---
+++ src/components/PageTool.vue
@@ -0,0 +1,9 @@
+<template>
+  <page-test v-if="category === 'test'"/>
+</template>
+
+<script setup lang="ts">
+import PageTest from "~/components/tool/PageTest.vue";
+
+const props = defineProps<{ category: string }>();
+</script>
