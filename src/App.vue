4210201150e1a9dfdf48a134aa8b0022950178e8
conglinyizhi <cslbyivi@outlook.com>
2024-08-20 21:08:55 +0800

    style: 调整代码格式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {useStore} from "~/store";
+import { useStore } from "~/store";
 import Main from "~/Main.vue";
 
 const store = useStore()
@@ -14,9 +14,7 @@ onMounted(async () => {
 
 <template>
   <div class="mx-auto w-full max-w-screen-lg h-full">
-    <el-watermark :content="testMode ? '仅用于展示，修改无效' : ''"
-                  :font="{fontSize: 24}"
-                  :gap="[100, 32]">
+    <el-watermark :content="testMode ? '仅用于展示，修改无效' : ''" :font="{ fontSize: 24 }" :gap="[100, 32]">
       <Main />
     </el-watermark>
   </div>
